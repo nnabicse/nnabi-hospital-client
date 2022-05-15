f654e73d4661d951bde7d021512acc9926bee543
Nurun Nabi <nnabi.cse@gmail.com>
2022-05-15 16:46:52 +0600

    services loaded from database

--- src/Pages/Appoinment/AvailableAppoinments.js
+++ src/Pages/Appoinment/AvailableAppoinments.js
@@ -7,7 +7,7 @@ const AvailableAppoinments = ({ date }) => {
     const [services, setServices] = useState([])
     const [treatment, setTreatment] = useState(null)
     useEffect(() => {
-        fetch(')
+        fetch('http://localhost:5000/service')
             .then(res => res.json())
             .then(data => setServices(data))
     }, [])
