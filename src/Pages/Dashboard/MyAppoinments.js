6029cb9c5f3257fa8105f449b3e0c0f7db895278
Nurun Nabi <nnabi.cse@gmail.com>
2022-05-16 22:09:43 +0600

    appointments for individual user loaded from database and dashboared into tabular form

--- src/Pages/Dashboard/MyAppoinments.js
+++ src/Pages/Dashboard/MyAppoinments.js
@@ -1,9 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
 
 const MyAppoinments = () => {
+    const [user] = useAuthState(auth)
+    const [appoinments, setAppoinments] = useState([])
+    useEffect(() => {
+        if (user) {
+            fetch(`http://localhost:5000/booking?patient=${user.email}`)
+                .then(res => res.json())
+                .then(data => setAppoinments(data));
+        }
+    }, [user])
     return (
         <div>
-            <h2>My Appointments</h2>
+            <h2>My Appointments: {appoinments.length}</h2>
+            <div class="overflow-x-auto">
+                <table class="table w-full">
+
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Name</th>
+                            <th>Date</th>
+                            <th>Time</th>
+                            <th>Treatment</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            appoinments.map(a => <tr>
+                                <th>1</th>
+                                <td>{a.patientName}</td>
+                                <td>{a.date}</td>
+                                <td>{a.slot}</td>
+                                <td>{a.treatment}</td>
+                            </tr>)
+                        }
+
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
